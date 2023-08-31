8c9b41a0a4812d6f0df21a0df5a918f357b3b5c9
aematei <alexanderematei@gmail.com>
2023-08-31 01:20:34 +0000

    Create Content component placeholder

---
+++ src/components/Content.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Content = () => {
+    return (
+      <div className="content">
+          <h1>Content goes here.</h1>
+      </div>
+    );
+  };
+  
+  export default Content;
