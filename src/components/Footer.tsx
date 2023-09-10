6d6c9ef105d2aff702169d1f52d156ee8ac96420
WhenitStarted <nteran13@gmail.com>
2023-09-10 16:52:00 -0700

    modify tailwind.config color section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 type Props = {};
 
 const Footer = (props: Props) =>{
@@ -8,15 +10,16 @@ const Footer = (props: Props) =>{
     <div
       className={`${flexBetween} fixed bottom-0 z-30 w-full py-6`}
     >
-        <div className = {`${flexBetween} w-full`}>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-
+        <div className = {`${flexBetween} w-full gap-8 text-sm bg-white `}>
+          <div className = {`${flexBetween} w-full `}>
+            <li className = {`${flexBetween} hover:electric-blue `}>About</li>
+            <li className = {`${flexBetween} hover:bg-sky-700 `}>Team</li>
+            <li className = {`${flexBetween} hover:bg-sky-700 `}>GitHub</li>
+            <li className = {`${flexBetween} hover:bg-sky-700 `}>What's New</li>
+            <li className = {`${flexBetween} hover:bg-sky-700 `}>Discord</li>
+          </div>
         </div>
-    
       </div>
-    
     </nav>
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "electric-blue": "#90E9EF"
+      }
+
+    },
   },
   plugins: [],
 }
