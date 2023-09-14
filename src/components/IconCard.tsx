1dd9ba9db36d43127f2cf6e1d94e2f558fb84d51
aematei <alexanderematei@gmail.com>
2023-09-14 22:54:30 +0000

    Create IconCardProps type along with enum for icon orientation

--- src/components/IconCard.tsx
+++ src/components/IconCard.tsx
@@ -1,1 +1,18 @@
 import React from "react";
+
+const enum Orientation {
+    north,
+    east,
+    south,
+    west
+}
+
+type IconCardProps = {
+    headingText: string,
+    bodyText: string,
+    iconSrc: string,
+    iconAlt: string,
+    orientation: Orientation,
+    bgColor: string
+
+}
