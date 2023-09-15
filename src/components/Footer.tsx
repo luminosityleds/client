095c784950acaaf458c29bc7c099af87aac7d7e8
WhenitStarted <nteran13@gmail.com>
2023-09-15 06:55:30 +0000

    Add icons to the footer in css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import About from ".../assets/About.svg"
-// import Discord from ".../assets/Team.svg"
-// import Github from "/assets/GitHub.svg"
-// import whatsNewIcon from "/assets/WhatsNew.svg"
-// import discordIcon from "/assets/Discord.svg"
+import aboutIcon from "../assets/About.svg"
+import teamIcon from "../assets/Team.svg"
+import githubIcon from "../assets/GitHub.svg"
+import whatsNewIcon from "../assets/WhatsNew.svg"
+import discordIcon from "../assets/Discord.svg"
 
 type Props = {};
 
@@ -11,15 +11,30 @@ const Footer = (props: Props) =>{
   const flexBetween = "flex justify-evenly";
   return <nav> 
     <div
-      className={`${flexBetween} fixed bottom-0 z-30 w-full py-6`}
+      className={`${flexBetween} fixed bottom-0  w-full pb-10`}
     >
         <div className = {`${flexBetween} w-full gap-8 text-sm bg-white `}>
-          <div className = {`${flexBetween} w-full text-footer-textSize pb-8`}>
-            <li className = {`${flexBetween} hover:text-electric-blue `}>About</li>
-            <li className = {`${flexBetween} hover:text-electric-blue `}>Team</li>
-            <li className = {`${flexBetween} hover:text-electric-blue `}>GitHub</li>
-            <li className = {`${flexBetween} hover:text-electric-blue `}>What's New</li>
-            <li className = {`${flexBetween} hover:text-electric-blue `}>Discord</li>
+          <div className = {`${flexBetween} w-full text-footer-textSize `}> 
+            <div className="flex hover:text-electric-blue">
+              <img className="h-7 mr-4  " src={aboutIcon} alt="About Icon"></img>             
+              <li className = {`${flexBetween}  `}>About</li>
+            </div>
+            <div className="flex hover:text-electric-blue">
+              <img className="h-7 mr-4  " src={teamIcon} alt="About Icon"></img>           
+              <li className = {`${flexBetween}  `}>Team</li>
+            </div>
+            <div className="flex hover:text-electric-blue">
+              <img className="h-7 mr-4  " src={githubIcon} alt="About Icon"></img>           
+              <li className = {`${flexBetween}  `}>GitHub</li>
+            </div>
+            <div className="flex hover:text-electric-blue">
+              <img className="h-7 mr-4  " src={whatsNewIcon} alt="About Icon"></img>           
+              <li className = {`${flexBetween}  `}>What's New</li>
+            </div>
+            <div className="flex hover:text-electric-blue">
+              <img className="h-7 mr-4  " src={discordIcon} alt="About Icon"></img>           
+              <li className = {`${flexBetween}  `}>Discord</li>
+            </div>
           </div>
         </div>
       </div>
