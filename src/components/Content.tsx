efa937e5fffddfc3ce8372fd41649b2f59af3137
aematei <alexanderematei@gmail.com>
2023-09-15 20:31:40 +0000

    Implement card orientation based on input

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,9 +4,15 @@ import sunlightIcon from "../assets/omega.svg"
 import signalIcon from "../assets/signal.svg"
 import barGraphIcon from "../assets/bar-graph.svg"
 
+var dummyHead = "What is Lorem Ipsum?"
+var dummyBody = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500"
+
 export const Content = () => {
     return (
-      <IconCard headingText="What is Lorem Ipsum?" bodyText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500" iconSrc={sunlightIcon} iconAlt="light" orientation={Orientation.north} bgColor="bg-electric-blue"/>
+      <div className="flex flex-col font-inter text-left">
+        <IconCard headingText={dummyHead} bodyText={dummyBody} iconSrc={sunlightIcon} iconAlt="light" orientation={Orientation.north} bgColor="bg-electric-blue"/>
+        <IconCard headingText={dummyHead} bodyText={dummyBody} iconSrc={signalIcon} iconAlt="signal" orientation={Orientation.east} bgColor="bg-white" />
+      </div>
       )
   }
   
--- src/components/IconCard.tsx
+++ src/components/IconCard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+//Tailwind Classes
 const HORIZONTAL_TEXT_WIDTH = "w-[50rem]"
 const VERTICAL_TEXT_WIDTH = "w-[24rem]"
+const HORIZONTAL_CENTER = "items-center"
+const VERTICAL_CENTER = "justify-center"
 
-export const enum Orientation { // orientation%2 gives 0 for vertical, 1 for horizontal
+export const enum Orientation { // orientation % 2 gives 0 for vertical, 1 for horizontal
     north,
     east,
     south,
@@ -20,12 +23,29 @@ type IconCardProps = {
 }
 
 export const IconCard = ({
-    headingText, bodyText, iconSrc, iconAlt, orientation, bgColor}: IconCardProps) => {
-    var size = (orientation % 2) ? VERTICAL_TEXT_WIDTH : HORIZONTAL_TEXT_WIDTH
+    headingText,
+    bodyText,
+    iconSrc,
+    iconAlt,
+    orientation,
+    bgColor}: IconCardProps) => {
+
+    var size, center, flexDirection
+
+    if (orientation % 2) {
+        size = VERTICAL_TEXT_WIDTH
+        center = VERTICAL_CENTER
+        flexDirection = "flex-row" + ((orientation===2) ? "reverse" : "")
+    }
+    else {
+        size = HORIZONTAL_TEXT_WIDTH
+        center = HORIZONTAL_CENTER
+        flexDirection = "flex-col" + ((orientation===1) ? "reverse" : "")
+    }
+
     return(
-        <div className={bgColor}>
+        <div className={`px-4 flex ${flexDirection} py-20 gap-4 ${bgColor} ${center}`}>
             {textContainer(headingText, bodyText, size)}
-            <p>{orientation}</p>
             <img src={iconSrc} alt={iconAlt}></img>
         </div>
     )
