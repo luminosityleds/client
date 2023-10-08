350629e27d9a0a1ab0afa449285fa4d5aecd8171
ALINZ22 <ALANMONGX@GMAIL.COM>
2023-10-07 22:02:33 -0700

    changed background colors and removed background from buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,23 @@
 export const Header = () => {
   return (
-    <nav className="nav flex justify-between items-center w-full bg-90E9EF text-black p-4">
+    <nav className="nav flex justify-between items-center w-full bg-[#90E9EF] text-black p-4">
       <div className="flex items-center space-x-4">
         <a href="/" className="site-logo hover:text-white">
           <img src="/images/luminosity-home-logo.ico" alt="Home-logo"></img>
         </a>
         <span className="text-lg font-semibold font-sans hover:text-white cursor-pointer">
           Luminosity LEDs
-        </span>{" "}
+        </span>
       </div>
 
       <ul className="flex space-x-4">
         <li>
-          <button className="px-4 py-2 bg-white text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
+          <button className="px-4 py-2 text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
             Log In
           </button>
         </li>
         <li>
-          <button className="px-4 py-2 bg-white text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
+          <button className="px-4 py-2 text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
             Sign Up
           </button>
         </li>
