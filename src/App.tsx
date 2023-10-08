8d116b41649f964187cc7d2006562318b3b80549
ALINZ22 <ALANMONGX@GMAIL.COM>
2023-10-07 21:47:49 -0700

    created header buttons and formatted everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import Content from './components/Content';
-import './App.css';
+import React from "react";
+import Content from "./components/Content";
+import Header from "./components/Header";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Content />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,29 @@
 export const Header = () => {
-  return <div className=""></div>;
+  return (
+    <nav className="nav flex justify-between items-center w-full bg-90E9EF text-black p-4">
+      <div className="flex items-center space-x-4">
+        <a href="/" className="site-logo hover:text-white">
+          <img src="/images/luminosity-home-logo.ico" alt="Home-logo"></img>
+        </a>
+        <span className="text-lg font-semibold font-sans hover:text-white cursor-pointer">
+          Luminosity LEDs
+        </span>{" "}
+      </div>
+
+      <ul className="flex space-x-4">
+        <li>
+          <button className="px-4 py-2 bg-white text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
+            Log In
+          </button>
+        </li>
+        <li>
+          <button className="px-4 py-2 bg-white text-black rounded hover:bg-A6EDF2 hover:text-white font-sans text-base">
+            Sign Up
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Header;
