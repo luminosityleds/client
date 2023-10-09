6c289a8a6d833634302fad101a4cabdc2e5c97cd
aematei <alexanderematei@gmail.com>
2023-10-09 02:12:46 +0000

    Install and configure babel macros vite plugin for fontawesome icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import macrosPlugin from "vite-plugin-babel-macros";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), macrosPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
