a7cbe1f89a46301751a715e072a4875fef88ce72
aematei <alexanderematei@gmail.com>
2023-10-15 17:50:39 +0000

    Initialize vitest config for unit testing and add a unit test

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/tests/setupTests.ts',
     css: true,
     reporters: ['verbose'],
     coverage: {
