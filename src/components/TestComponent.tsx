b590dff1a1bf3b0cd8e822954731e76632348f5a
aematei <alexanderematei@gmail.com>
2023-10-28 15:00:14 +0000

    Add post interface and default value

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,7 +1,18 @@
+import axios from 'axios';
+
 export const TestComponent = () => {
     return (
-        <h1> Mock API Tests </h1>
+        <h1> Placeholder API Tests </h1>
     )
 }
+
+interface IPost {
+    id: number;
+    userId?: number;
+    title: string;
+    body: string;
+  }
+
+const defaultPosts:IPost[] = [];
 
 export default TestComponent;
