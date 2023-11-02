2293ad70fb485abfe25e1bb42dd61d6d6a16f08f
Jihnboii <rodriguezjohn910@gmail.com>
2023-11-01 21:16:41 -0700

    Create Login and Register buttons and forgot password

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,28 @@
+import Header from "./Header";
+import { useToggle } from "../ts/ToggleHeader";
+
 export const Login = () => {
+    const lightMode = useToggle((state) => state.light);
     return(
-        <h1>Login Page</h1>
+        <div className={`"App" ${lightMode ? "" : "dark"}` }>
+            <Header />
+            <div className="flex flex-col items-center justify-center h-screen">
+                <ul>
+                    <li>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Login with Github
+                        </button>
+                    </li>
+                    <li>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Login with Google
+                        </button>  
+                    </li>
+                </ul>
+                <p>Forgot Password? | Register</p>
+            </div>
+        </div>
+        
     )
 }
 
