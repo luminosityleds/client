dbfcb826ca06fa81d89c688bd4f7f0cf2e5dfd31
Jihnboii <rodriguezjohn910@gmail.com>
2023-11-11 12:01:50 -0800

    Added lightmode and darkmode to login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,54 @@
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import Header from "./Header";
 import { useToggle } from "../ts/ToggleHeader";
 
 export const Login = () => {
     const lightMode = useToggle((state) => state.light);
+
+
     return(
-        <div className={`"App" ${lightMode ? "" : "dark"}` }>
+        <div className={`"App" ${lightMode ? "bg-card-light-blue" : "dark bg-card-dark-blue"}` }>
             <Header />
             <div className="flex flex-col items-center justify-center h-screen">
                 <ul>
                     <li>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Login with Github
+                        <button 
+                            className={`${
+                                lightMode
+                                    ? "bg-white text-black"
+                                    : "bg-black text-white"
+                             } w-72 h-12 py-2 px-4 rounded mb-12`}
+                        >
+                        <FontAwesomeIcon
+                        icon={icon({ name: 'github', style: 'brands' })}
+                        
+                        /> Log In with Github
                         </button>
                     </li>
                     <li>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Login with Google
+                        <button 
+                            className={`${
+                                lightMode 
+                                    ? "bg-white text-black"
+                                    : "bg-black text-white" 
+                            } w-72 h-12 py-2 px-4 rounded mt-12 mb-8`}
+                        >
+                        <FontAwesomeIcon
+                        icon={icon({ name: 'google', style: 'brands' })}
+                        /> Log In with Google
                         </button>  
                     </li>
                 </ul>
-                <p>Forgot Password? | Register</p>
+                <p 
+                    className={`${
+                        lightMode
+                            ? "text-black"
+                            : "text-white" 
+                    } mt-8`}
+                >
+                    Forgot Password? | Register
+                </p>
             </div>
         </div>
         
