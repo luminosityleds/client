3900c0ef349d9b84fa7c7d2611c5071b7bc1b8ff
Jihnboii <rodriguezjohn910@gmail.com>
2023-11-15 19:00:29 -0800

    Remove Scroll Bar

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,38 +8,36 @@ export const Login = () => {
 
 
     return(
-        <div className={`"App" ${lightMode ? "bg-card-light-blue" : "dark bg-card-dark-blue"}` }>
+        <div className={`"App" ${lightMode ? "bg-card-light-blue" : "dark bg-card-dark-blue"} min-h-screen flex flex-col`}>
             <Header />
-            <div className="flex flex-col items-center justify-center h-screen">
-                <ul>
-                    <li>
-                        <button 
-                            className={`${
-                                lightMode
-                                    ? "bg-white text-black"
-                                    : "bg-black text-white"
-                             } w-72 h-12 py-2 px-4 rounded mb-12`}
-                        >
+            <div className="relative flex flex-grow">
+            <div className="flex flex-col items-center justify-center flex-1">
+                
+                    <button 
+                        className={`${
+                            lightMode
+                                ? "bg-white text-black"
+                                : "bg-black text-white"
+                            } w-[720px] h-[120px] mt-[240px] py-2 px-4 rounded mb-12`}
+                    >
                         <FontAwesomeIcon
                         icon={icon({ name: 'github', style: 'brands' })}
                         
                         /> Log In with Github
                         </button>
-                    </li>
-                    <li>
+                  
                         <button 
                             className={`${
                                 lightMode 
                                     ? "bg-white text-black"
                                     : "bg-black text-white" 
-                            } w-72 h-12 py-2 px-4 rounded mt-12 mb-8`}
+                            } w-[720px] h-[120px] py-2 px-4 rounded mt-12 mb-8`}
                         >
                         <FontAwesomeIcon
                         icon={icon({ name: 'google', style: 'brands' })}
                         /> Log In with Google
                         </button>  
-                    </li>
-                </ul>
+               
                 <p 
                     className={`${
                         lightMode
@@ -50,8 +48,8 @@ export const Login = () => {
                     Forgot Password? | Register
                 </p>
             </div>
+            </div>
         </div>
-        
     )
 }
 
