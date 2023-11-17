309d557e0bd141d32f19e57e3fb014120eb66781
Jihnboii <rodriguezjohn910@gmail.com>
2023-11-16 22:43:20 -0800

    //Created Login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,49 +7,57 @@ export const Login = () => {
     const lightMode = useToggle((state) => state.light);
 
 
-    return(
-        <div className={`"App" ${lightMode ? "bg-card-light-blue" : "dark bg-card-dark-blue"} min-h-screen flex flex-col`}>
-            <Header />
-            <div className="relative flex flex-grow">
+    return(   
+    <div className={`"App" ${lightMode ? "bg-card-light-blue" : "dark bg-card-dark-blue"} min-h-screen flex flex-col`}>
+        <Header /> 
+        <div className="relative flex flex-grow">
             <div className="flex flex-col items-center justify-center flex-1">
-                
-                    <button 
-                        className={`${
-                            lightMode
-                                ? "bg-white text-black"
-                                : "bg-black text-white"
-                            } w-[720px] h-[120px] mt-[240px] py-2 px-4 rounded mb-12`}
+                <button 
+                    className={`${ 
+                        lightMode
+                            ? "bg-duke-blue"
+                            : "bg-black" 
+                    } text-white w-[720px] h-[120px] py-2 px-12 rounded mb-32 flex items-center justify-center`}
+                >
+                    <FontAwesomeIcon
+                    icon={icon({ name: 'github', style: 'brands' })}
+                    className="mr-2 h-12" 
+                    /> 
+                    <span className="flex-grow text-base">
+                        <p className="text-[48px]">Log In with Github</p>
+                        </span>
+                </button>
+                    
+                    
+                <button 
+                    className={`${
+                        lightMode 
+                            ? "bg-duke-blue"
+                            : "bg-black" 
+                    } text-white w-[720px] h-[120px] py-2 px-12 rounded mb-16 flex items-center justify-center`}
                     >
-                        <FontAwesomeIcon
-                        icon={icon({ name: 'github', style: 'brands' })}
-                        
-                        /> Log In with Github
-                        </button>
-                  
-                        <button 
-                            className={`${
-                                lightMode 
-                                    ? "bg-white text-black"
-                                    : "bg-black text-white" 
-                            } w-[720px] h-[120px] py-2 px-4 rounded mt-12 mb-8`}
-                        >
-                        <FontAwesomeIcon
-                        icon={icon({ name: 'google', style: 'brands' })}
-                        /> Log In with Google
-                        </button>  
-               
+                    <FontAwesomeIcon
+                    icon={icon({ name: 'google', style: 'brands' })}
+                    className="mr-2 h-12" 
+                    /> 
+                    <span className="flex-grow text-base">
+                    <p className="text-[48px]">Log In with Google</p>
+                        </span>
+                </button>  
+                    
                 <p 
                     className={`${
                         lightMode
-                            ? "text-black"
+                            ? "text-duke-blue"
                             : "text-white" 
-                    } mt-8`}
-                >
-                    Forgot Password? | Register
+                    } text-[24px] mb-10`}
+            >
+                Forgot Password? | Register
                 </p>
             </div>
-            </div>
         </div>
+    </div>
+        
     )
 }
 
