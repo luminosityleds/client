d26790b2e61c1a5a10d707b2eed579bae639d14a
Jihnboii <rodriguezjohn910@gmail.com>
2023-11-26 11:48:20 -0800

    -Added hover to buttons and defined some new color

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -15,8 +15,8 @@ export const Login = () => {
                 <button 
                     className={`${ 
                         lightMode
-                            ? "bg-electric-blue border-black hover:bg-card-light-blue"
-                            : "bg-black text-white hover:bg-suva-grey" 
+                            ? "bg-card-sky-blue border-black hover:bg-electric-blue"
+                            : "bg-black text-white hover:bg-charcoal-black" 
                     } border w-[720px] h-[120px] py-2 px-12 rounded-lg mb-24 flex items-center justify-center`}
                 >
                     <FontAwesomeIcon
@@ -24,7 +24,11 @@ export const Login = () => {
                     className="mr-2 h-12" 
                     /> 
                     <span className="flex-grow text-base">
-                        <p className="text-[48px] ml-2">Log In with Github</p>
+                        <p className={`${
+                            lightMode
+                                ? "text-duke-blue"
+                                : "text-white"
+                        } text-[48px] ml-2`}>Log In with Github</p>
                         </span>
                 </button>
                     
@@ -32,8 +36,8 @@ export const Login = () => {
                 <button 
                     className={`${
                         lightMode 
-                            ? "bg-electric-blue border-black hover:bg-card-light-blue"
-                            : "bg-black text-white hover:bg-suva-grey" 
+                            ? "bg-card-sky-blue border-black hover:bg-electric-blue"
+                            : "bg-black text-white hover:bg-charcoal-black" 
                     } border w-[720px] h-[120px] py-2 px-12 rounded-lg mb-12 flex items-center justify-center`}
                     >
                     <FontAwesomeIcon
@@ -41,7 +45,11 @@ export const Login = () => {
                     className="mr-2 h-12" 
                     /> 
                     <span className="flex-grow text-base">
-                    <p className="text-[48px] ml-2">Log In with Google</p>
+                    <p className={`${
+                            lightMode
+                                ? "text-duke-blue"
+                                : "text-white"
+                        } text-[48px] ml-2`}>Log In with Google</p>
                         </span>
                 </button>  
                     
@@ -50,21 +58,21 @@ export const Login = () => {
                 <span
                     className={`${
                         lightMode
-                            ? "text-duke-blue hover:text-electric-blue"
+                            ? "text-duke-blue hover:text-sapphire-blue"
                             : "text-white hover:text-suva-grey" 
                     }`}
                 >Forgot Password? </span> 
                 <span 
                     className={`${
                         lightMode
-                            ? "text-electric-blue"
-                            : "text-suva-grey" 
+                            ? "text-duke-blue"
+                            : "text-white" 
                     }`}
                 >|</span> 
                 <span
                     className={`${
                         lightMode
-                            ? "text-duke-blue hover:text-electric-blue"
+                            ? "text-duke-blue hover:text-sapphire-blue"
                             : "text-white hover:text-suva-grey" 
                     }`}
                 > Register</span>
