b582ee9ad4717c2a88b5466583413ce7865fb264
aematei <alexanderematei@gmail.com>
2024-02-20 21:14:33 +0000

    wip: use hook to get todos
    Signed-off-by: aematei <alexanderematei@gmail.com>

--- src/components/ExampleServicePage.tsx
+++ src/components/ExampleServicePage.tsx
@@ -1,3 +1,22 @@
+import example from "../microservices/example.service"
+import IExampleData from "../ts/IExampleData";
+import { create } from 'zustand'
+
+interface ToDoState {
+  todos: IExampleData[]
+  refresh: () => void
+}
+
+const useBearStore = create<ToDoState>()((set) => ({
+  todos: [],
+  refresh: () => set((state) => ({ todos: example.getAll()})),
+}))
+
+
+let data = await example.getAll();
+for (var datum of data) {
+    console.log(datum)
+}
 export const ExampleServicePage = () => {
     return (
         <>
