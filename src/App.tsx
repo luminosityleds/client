8dc79323ab8d8b605acca0177331bc39f322fb36
ALINZ22 <ALANMONGX@GMAIL.COM>
2024-02-23 12:58:34 -0800

    add routing function to register page
    Signed-off-by: ALINZ22 <ALANMONGX@GMAIL.COM>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import FrontPage from "./components/FrontPage";
 import Login from "./components/Login";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Register from "./components/Register";
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<FrontPage/>} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<FrontPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
