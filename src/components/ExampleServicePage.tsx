90ccfbc789702bf8b758eb2428a9fe512c2e99a7
aematei <alexanderematei@gmail.com>
2024-02-25 22:48:01 +0000

    ip: ts logic for state mgmt
    Signed-off-by: aematei <alexanderematei@gmail.com>

--- src/components/ExampleServicePage.tsx
+++ src/components/ExampleServicePage.tsx
@@ -2,25 +2,57 @@ import example from "../microservices/example.service"
 import IExampleData from "../ts/IExampleData";
 import { create } from 'zustand'
 
+
+interface CounterState {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+const useCounterStore = create<CounterState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+}));
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 interface ToDoState {
-  todos: IExampleData[]
-  refresh: () => void
+  todos: Todo[];
+  setOne: (id: number) => void;
+  setAll: () => void;
 }
 
-const useBearStore = create<ToDoState>()((set) => ({
-  todos: [],
-  refresh: () => set((state) => ({ todos: example.getAll()})),
-}))
-
-
-let data = await example.getAll();
-for (var datum of data) {
-    console.log(datum)
+interface PlaceholderState {
+  todos: any;
+  set: () => void
 }
+const usePlaceholderStore = create<PlaceholderState>((set) => ({
+  todos: 0,
+  set: () => {todo: example.getAll(); console.log(example.getAll())}
+}));
+
+const ToDos = () => {
+  const { count, increment, decrement } = useCounterStore();
+  const { todos, set } = usePlaceholderStore();
+
+  return (
+    <div>
+      <button onClick={set}>Get Todos</button>
+      <h1>TODOS</h1>
+    </div>
+  );
+};
+
 export const ExampleServicePage = () => {
+
     return (
         <>
-        Test.
+        <ToDos/>
         </>
     )
 
